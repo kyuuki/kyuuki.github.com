7c86ff448a1c31a70a8c2310f35a0c9113d0cec3
kyuuki <fdear.kobayashi@nifty.com>
2013-05-17 19:06:04 +0900

    Git リポジトリの詳細がある場合の表示を変更。 Git リポジトリ読み込み部分 (コピペしてきたもの) のタブサイズを 2 → 4 に変更。

--- js/main.js
+++ js/main.js
@@ -3,34 +3,37 @@ $(document).ready(function(){
 });
 
 $(function(){
-  $.ajax({
-    type: 'GET',
-    url: 'https://api.github.com/users/kyuuki/repos',
-    cache: false,
-    dataType: 'jsonp',
-    success: function(data) {
-      var datas, i, l, repoElement, reposElement, desc;
-      datas = data.data;
-      reposElement = $('#repositories ul');
-      for(i=0, l=datas.length; i<l; i++) {
-        data = datas[i];
-        repoElement = $('<li><a class="url"></a><span class="desc" /></li>');
-        repoElement.find('a').attr('href', data.html_url).html(data.name);
-        desc = (data.description || '').slice(0, 20);
-        if(desc != (data.description || '')) {
-          desc += '...';
-        }
-        repoElement.find('.desc').text(desc);
-        if(data.fork){
-          repoElement.addClass('fork');
-        }
+    $.ajax({
+        type: 'GET',
+        url: 'https://api.github.com/users/kyuuki/repos',
+        cache: false,
+        dataType: 'jsonp',
+        success: function(data) {
+            var datas, i, l, repoElement, reposElement, desc;
+            datas = data.data;
+            reposElement = $('#repositories ul');
+            for (i = 0, l = datas.length; i < l; i++) {
+                data = datas[i];
+                repoElement = $('<li><a class="url"></a><span class="desc" /></li>');
+                repoElement.find('a').attr('href', data.html_url).html(data.name);
+                desc = (data.description || '').slice(0, 20);
+                if (desc != (data.description || '')) {
+                    desc += '...';
+                }
+                if (data.description) {
+                    repoElement.find('.desc').before(" - ");
+                }
+                repoElement.find('.desc').text(desc);
+                if (data.fork) {
+                    repoElement.addClass('fork');
+                }
 
-        if(data.language){
-          repoElement.addClass('lang-' + data.language.toLowerCase());
-        }
+                if (data.language) {
+                    repoElement.addClass('lang-' + data.language.toLowerCase());
+                }
 
-        reposElement.append(repoElement);
-      }
-    }
-  });
+                reposElement.append(repoElement);
+            }
+        }
+    });
 });
